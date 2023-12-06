f05c35b41fef772acef1b0d88f6ac8c3c9242485
vmsree <133580730+vmsree@users.noreply.github.com>
2023-12-06 19:02:16 +0530

    Create Set-Operations.py

---
+++ Set-Operations.py
@@ -0,0 +1,12 @@
+s1 = {1,2,3,4}
+s2 = {2,4,7,8,9}
+
+Union = s1.union(s2)
+Difference = s1.difference(s2)
+Intersection = s1.intersection(s2)
+Symmetric_difference = s1.symmetric_difference(s2)
+
+print("Union of sets: ", Union)
+print("Difference of sets: ", Difference)
+print("Intersection of sets: ", Intersection)
+print("Symmetric_difference of sets: ", Symmetric_difference)
